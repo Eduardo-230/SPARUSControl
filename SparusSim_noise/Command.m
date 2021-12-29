b3091a6ea9b9047dcbce76805a4f285afd3dc7ea
Eduardo-230 <96792014+Eduardo-230@users.noreply.github.com>
2021-12-29 20:00:17 +0100

    Added PD to control pos at last phase

--- SparusSim_noise/Command.m
+++ SparusSim_noise/Command.m
@@ -12,19 +12,22 @@ thrust_h  = -Kp*delta_h+Kd*VitB(3);
 
 
 
-if (delta_h<0.2&delta_h>-0.2&PosE(1)<12) %if  SParus arrived to a depth of 5m, move foward 20m. 
-set_s = 1;
-Kd =  100;
-Kp = 260;
-Ki = 0.3;
-delta_s = set_s - VitB(1);
-thurst_s = -Kp*delta_s + Kd*AccB(1) - Ki*PosE(1);
+if (delta_h<0.2&delta_h>-0.2&PosE(1)<19) %if  SParus arrived to a depth of 5m, move foward 20m. 
+    set_vs = 1;
+    Kd =  100;
+    Kp = 360;
+    Ki = 0.3;
+    delta_s = set_vs - VitB(1);
+    thrust_s = -Kp*delta_s + Kd*AccB(1) - Ki*PosE(1);
 
-elseif (PosE(1)>20&PosE(1)<20.4)  %short (400ms) reverse pulse on motors to break inertia
-    thurst_s = 11;
-
+elseif (PosE(1)>19)  
+    set_s = 20;
+    Kd = 180;
+    Kp = 25;
+    delta_s = set_s - PosE(1);
+    thrust_s  = -Kp*delta_s+Kd*VitB(1);
 else                   
-    thurst_s = 0;
+    thrust_s = 0;
 end    
-Thrust=[thrust_h thurst_s thurst_s];
+Thrust=[thrust_h thrust_s thrust_s];
 Out=Thrust;
